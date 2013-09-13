0187d7193eaafd907ce3203585a0c63acdf1823a
Adam Horne <ahorne@wordplaysoftware.com>
2013-09-13 15:28:14 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TemplateBootstrapSite::Application.config.secret_key_base = 'a4ecef447b26f8e8f43db3425305a6163ddb41f25e94e63c315a2721468a41a862f70b69f433f77f15fe56af1daebb4ba0c985149a45bf14923a5068d6736134'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TemplateBootstrapSite::Application.config.session_store :cookie_store, key: '_template_bootstrap_site_session'
